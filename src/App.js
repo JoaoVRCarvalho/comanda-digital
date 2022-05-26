2391aadf69d677c3974f04ef680a66aa641eff24
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-05-25 22:29:30 -0300

    Routes still don't work! :(

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
+import Header from "./components/Navbar/Navbar";
 // import Login from './pages/Login'
 import Homepage from './pages/Homepage'
+import CadatroProduto from './pages/CadastroProduto'
+import CadatroFuncionario from './pages/CadastroFuncionario'
+import CadastroPrato from "./pages/CadastroPratos";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
     return (
         <Router>
-            <Homepage/>
-
+            <Header />
             <Routes>
-                <Route path="/">
-                    
-                </Route>
+                <Route exact path="/" component={Homepage}/>
+                <Route path="/produto" component={CadatroProduto}/>
+                <Route path="/funcionario" component={CadatroFuncionario}/>
+                <Route path="/prato" component={CadastroPrato}/>
             </Routes>
         </Router>
     )
