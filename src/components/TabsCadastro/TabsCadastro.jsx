67f5b7e22b283982b3d12b9757b6b19df1542239
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-06-02 16:38:48 -0300

    Added tab styling, logic and data

---
+++ src/components/TabsCadastro/TabsCadastro.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./TabsCadastro.css"
+import { TabsData } from "./TabsData";
+import { Tabs, Tab } from "react-bootstrap";
+
+export default function TabsCadastro() { //iterar sob o item antes de colar aqui
+    return (
+        <Tabs>
+            {TabsData.map((data, index) => (
+                <Tab className="mb-3"
+                    key={index} 
+                    eventKey={index} 
+                    title={data.title}>
+                </Tab>
+            ))}
+        </Tabs>
+    )
+}
---
+++ src/components/TabsCadastro/TabsData.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const TabsData = [
+    {
+        title: "Cadastro de Produtos",
+        key: "produtos",
+        cName: "tabCadastro",
+        tabInputComponent: <>oi</>,
+    },
+    {
+        title: "Cadastro de Pratos",
+        key: "produtos",
+        cName: "tabCadastro",
+        tabInputComponent: <>oi</>,
+    },
+    {
+        title: "Cadastro de funcionarios",
+        key: "produtos",
+        cName: "tabCadastro",
+        tabInputComponent: <>oi</>,
+    },
+]
