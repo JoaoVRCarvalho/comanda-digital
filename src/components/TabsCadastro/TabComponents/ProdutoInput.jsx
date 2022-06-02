b91fef78ee3cc410afb0003730c21891e6240b35
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-06-02 16:37:26 -0300

    Form component for the Cadastro Produto tab

---
+++ src/components/TabsCadastro/TabComponents/ProdutoInput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Form } from "react-bootstrap";
+
+export default function ProdutoInput() {
+    return (
+        <Container>
+            <Form>
+                
+            </Form>
+        </Container>
+    )
+}
