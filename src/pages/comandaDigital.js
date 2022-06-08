7a04582b53fc9439d9d9f7b5f2e976e74f7b2e51
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-06-07 21:41:41 -0300

    Pagina da comanda

---
+++ src/pages/comandaDigital.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ComandaComponent from "../components/Comanda/ComandaComponent.jsx"
+
+const ComandaDigital = () => (
+    <ComandaComponent/>
+)
+
+export default ComandaDigital;
