23e654b6f98201e7baa4c04f29941113a604e9c7
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-06-07 21:37:10 -0300

    Added new route to comanda

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Navbar/Navbar";
 import Login from './pages/Login';
 import Homepage from './pages/Homepage';
 import Cadatros from './pages/CadastroProduto';
+import ComandaDigital from './pages/comandaDigital';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
             <Header/>
             <Routes>
                 <Route exact path="/" element={<Homepage/>}/>
+                <Route path="/comanda" element={<ComandaDigital/>}/>
                 <Route path="/cadastro" element={<Cadatros/>}/>
                 <Route path="/Login" element={<Login/>}/>
             </Routes>
