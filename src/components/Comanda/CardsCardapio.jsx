0f68970a5ad958c9ee1eb23f498f075bfdb7addf
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-06-14 17:14:42 -0300

    Minor tweaks

--- src/components/Comanda/CardsCardapio.jsx
+++ src/components/Comanda/CardsCardapio.jsx
@@ -1,22 +1,24 @@
 import { React } from 'react';
 import { listaProdutos } from '../../dbMock/mockTables.js'
-import { Card, Container } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 
 export default function CardapioCards() {
     return (
-        <Container>
-            {listaProdutos.map((obj, index) => {
-                <Card key={obj.index} className="bg-dark text-white">
-                    <Card.Img src={obj.prodImg} alt="Card image" />
-                        <Card.ImgOverlay>
-                        <Card.Title>{obj.nome}</Card.Title>
-                            <Card.Text>
-                                {obj.Ds_Produto}
-                            </Card.Text>
-                    </Card.ImgOverlay>
-                </Card>
-            })}
-        </Container>
+            <Row xs={1} md={2} className="g-4">
+                {listaProdutos.map((obj, idx) => (
+                    <Col key={idx}>
+                        <Card key={obj.id} className="bg-dark text-white">
+                            <Card.Img src={obj.prodImg} alt="Card image" />
+                                <Card.ImgOverlay>
+                                    <Card.Title>{obj.nome}</Card.Title>
+                                    <Card.Text>
+                                        {obj.Ds_Produto}
+                                    </Card.Text>
+                                </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
     )
 }
 
--- src/dbMock/mockTables.js
+++ src/dbMock/mockTables.js
@@ -1,4 +1,3 @@
-import React from "react";
 // fazer mock com todas as infos de protudos
 
 export const listaProdutos = [
