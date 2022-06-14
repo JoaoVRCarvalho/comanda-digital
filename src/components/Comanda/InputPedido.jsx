43e495b0e812d6c44b0955aec675bf28c8acf4dc
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-06-13 21:19:03 -0300

    Added input logic for the comanda

--- src/components/Comanda/InputPedido.jsx
+++ src/components/Comanda/InputPedido.jsx
@@ -1,16 +1,23 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import { Container, Button } from 'react-bootstrap';
+import Cardapio from './Cardapio';
 
 export default function InputPedido() {
+    let [item, setItem] = useState(false)
+    const addItem = () => {
+        setItem(!item);
+    }
     return (
-        // aperto o botão, aparaece os inputs para adicionar item ao pedido,
-        // um do inputs ja aparece ao clicar aós isso o garçcom deve apertar o btn
-        // adicionar item, assim fazendo aparecer mais um input, na parte mais a direita do input
-        // havera um X para tirar tal input.
-        <Container className='pedidoInputDiv'>
-
-            <Button className="comandaBtn" variant='warning'>Adicionar item</Button>
-        </Container>
+        <>
+            <Container className="CardapioContainer">
+                {item &&
+                    <Cardapio/>
+                }
+            </Container>
+            <Container className='pedidoInputDiv'>
+                <Button className="comandaBtn" variant='warning' onClick={addItem}>Adicionar item</Button>
+            </Container>    
+        </>
     )
 }
 
