cc5d03a94e2bafe69de5bf9b30101ee4a92e637f
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-06-22 17:16:05 -0300

    order list

---
+++ src/components/Comanda/ListaPedido.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+// import { ListGroup } from "react-bootstrap";
+
+// export default function ListaPedido({ listaPedidos }) {
+//     <ListGroup className="my-2">
+//         <ListGroup.Item>This ListGroup</ListGroup.Item>
+//         <ListGroup.Item>renders horizontally</ListGroup.Item>
+//         <ListGroup.Item>on</ListGroup.Item>
+//         <ListGroup.Item>and above!</ListGroup.Item>
+//     </ListGroup>
+// }
