5e1e86501bb1dee33a9efe17c54d3c557a6182b9
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-06-23 16:01:34 -0300

    scrapped component

--- src/components/Comanda/_ComandaHeader.jsx
+++ src/components/Comanda/_ComandaHeader.jsx
@@ -1,33 +1,35 @@
-import React, { useState } from "react";
-import { useLocation } from 'react-router-dom';
-import LogoutButton from "../Navbar/Logout";
-import MenuButton from "../Navbar/MenuButton";
-import { SidebarComanda } from "../sidebar/sidebar";
-import '../Navbar/Navbar.css';
+// DEPRECIADO
 
-const ComandaHeader = ({ pedidosArr }) => {
-    const [sidebar, setSidebar] = useState(false);
-    const GetLocation = () => (useLocation().pathname);
+// import React, { useState } from "react";
+// import { useLocation } from 'react-router-dom';
+// import LogoutButton from "../Navbar/Logout";
+// import MenuButton from "../Navbar/MenuButton";
+// import { SidebarComanda } from "../sidebar/sidebar";
+// import '../Navbar/Navbar.css';
 
-    const showSidebar = () => setSidebar(!sidebar);
-    return (
-        <header id="testeTestado" className="app-header">
-            <div className="header-wrapper">
-                <div className="headerLeft">
-                    <div name="Sidebar" className="icon-container">
-                        <MenuButton showSidebar={showSidebar}/>
-                    </div>
-                    <span className="header-logo">AdrianoLancheS2</span>
-                </div>
-                <div className="headerRight">
-                    <div name="Logout" className="icon-container">
-                        <LogoutButton />
-                    </div>
-                </div>
-            </div>
-            <SidebarComanda listaPedidos={pedidosArr} sidebar={sidebar} showSidebar={showSidebar}/>
-        </header>
-    )
-}
+// const ComandaHeader = ({ pedidosArr }) => {
+//     const [sidebar, setSidebar] = useState(false);
+//     const GetLocation = () => (useLocation().pathname);
 
-export default ComandaHeader;
+//     const showSidebar = () => setSidebar(!sidebar);
+//     return (
+//         <header id="testeTestado" className="app-header">
+//             <div className="header-wrapper">
+//                 <div className="headerLeft">
+//                     <div name="Sidebar" className="icon-container">
+//                         <MenuButton showSidebar={showSidebar}/>
+//                     </div>
+//                     <span className="header-logo">AdrianoLancheS2</span>
+//                 </div>
+//                 <div className="headerRight">
+//                     <div name="Logout" className="icon-container">
+//                         <LogoutButton />
+//                     </div>
+//                 </div>
+//             </div>
+//             <SidebarComanda listaPedidos={pedidosArr} sidebar={sidebar} showSidebar={showSidebar}/>
+//         </header>
+//     )
+// }
+
+// export default ComandaHeader;
