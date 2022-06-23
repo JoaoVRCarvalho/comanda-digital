cf35d283fdc299633874573cf0a96daf893ee259
JoaoVRCarvalho <vitorrordriguesjc@gmail.com>
2022-06-22 21:34:52 -0300

    order array now working

--- src/components/Comanda/ListaPedido.jsx
+++ src/components/Comanda/ListaPedido.jsx
@@ -1,11 +1,38 @@
-// import React from "react";
-// import { ListGroup } from "react-bootstrap";
+import React from "react";
+import { ListGroup, Button } from "react-bootstrap";
+import { CloseRounded } from '@mui/icons-material';
 
-// export default function ListaPedido({ listaPedidos }) {
-//     <ListGroup className="my-2">
-//         <ListGroup.Item>This ListGroup</ListGroup.Item>
-//         <ListGroup.Item>renders horizontally</ListGroup.Item>
-//         <ListGroup.Item>on</ListGroup.Item>
-//         <ListGroup.Item>and above!</ListGroup.Item>
-//     </ListGroup>
-// }
+
+export default function Pedidos ({ listaPedidos }) {
+    
+    const handleClick = (objFromPedido) => {
+        listaPedidos.findIndex((obj, idx) => {
+            console.log(objFromPedido.id);
+            console.log(obj.id);
+            if (obj.id == objFromPedido.id) {
+                listaPedidos.splice(idx, 1)
+            }
+        })
+    }
+    
+    return (
+        <>
+            {listaPedidos.map((obj, idx) => (
+                <ListGroup className="list-row-wrapper" key={idx} horizontal="sm">
+                    <ListGroup.Item className="list-row">{obj.nome}</ListGroup.Item>
+                    <ListGroup.Item className="list-row">{obj.valor_produto}</ListGroup.Item>
+                    <ListGroup.Item className="list-row" id="remove-button-list">
+                        <Button 
+                            id="removeFromListButton"
+                            variant="warning" 
+                            className="comandaBtn"
+                            onClick={e => {handleClick(obj)}}>
+                            <CloseRounded/>
+                        </Button>
+                    </ListGroup.Item>
+                </ListGroup>
+            ))}
+        </>
+    )
+
+}
