2f0bba537eda19b2f33846562b946547ec130a71
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2022-06-25 13:58:36 -0300

    removed unused assets

--- src/components/Comanda/InputPedido.jsx
+++ src/components/Comanda/InputPedido.jsx
@@ -4,31 +4,7 @@ import Pedidos from './ListaPedido.jsx';
 
 
 export default function InputPedido({ idMesa }) {
-    const [item, setItem] = useState(0)
     const [pedidoArr, setPedidoArr] = useState([]);
-    // const buffer = [];
-    let counter = item;
-
-    // const handleAddItem = (e) => {
-    //     if (e) {
-    //         counter++
-    //         setItem(counter);
-    //     }
-    // }
-
-    // if (item >= 1) {
-    //     for (let i = 0; i < item; i++) {
-    //         buffer.push(
-    //             <Cardapio 
-    //                 idMesa={idMesa}
-    //                 pedidoArr={pedidoArr} 
-    //                 setPedidoArr={setPedidoArr}
-    //                 cardapiosToRender={item}
-    //                 setCardapiosToRender={setItem}
-    //             />
-    //         );
-    //     }
-    // }
 
     return (
         <div className="pedido-wrapper">
@@ -38,30 +14,7 @@ export default function InputPedido({ idMesa }) {
                 idMesa={idMesa}
                 pedidoArr={pedidoArr} 
                 setPedidoArr={setPedidoArr}
-                cardapiosToRender={item}
-                setCardapiosToRender={setItem}
             />
-            
-            {/* <Container className="CardapioContainer">
-                {buffer.map((cardapio, index) => {
-                    return (
-                        <div key={index}>
-                            <div>
-                                {cardapio}
-                            </div>
-                        </div>
-                    )
-                })}
-            </Container>
-            <Container className='pedidoInputDiv'>
-                <Button
-                    className="comandaBtn" 
-                    id='addItem-btn' 
-                    variant='warning' 
-                    onClick={e => handleAddItem(e)}>
-                        Novo item
-                </Button>
-            </Container>     */}
         </div>
     )
 }
